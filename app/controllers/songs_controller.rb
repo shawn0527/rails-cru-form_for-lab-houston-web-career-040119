842bb566444ff8cee1507fb4f84461f25b258b52
Shawn Ren <shawnren527@gmail.com>
2019-04-29 05:39:26 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,37 @@
+class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = current_song
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    song = Song.create(song_params)
+    redirect_to song_path(song)
+  end
+
+  def edit
+    @song = current_song
+  end
+
+  def update
+    song = current_song
+    song.update(song_params)
+    redirect_to song_path(song)
+  end
+
+  private
+  def current_song
+    Song.find(params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit(:name, :artist_id, :genre_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :artists, :songs, :genres, only: [:show, :new, :create, :edit, :update]
+  get '/songs', to: 'songs#index'
 end
